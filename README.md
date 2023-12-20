290ab0c98ccfeaaf44b585f00615b0527148cbc6
Yovel Key-Cohen <ykey-cohen@emeryweiner.org>
2023-12-19 19:38:52 -0800

    v3

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Vaults v2
-Simple Python script for making and managing encrypted vaults. Vaults use symmetrically encrypted RSA keys to encode and decode data.
+# Vaults v3
+Simple Python script for making and managing encrypted vaults. Vaults use a symmetrically encrypted, randomly generated AES key to encode and decode data.
 
 Requires pycryptodome (and opencv-python if you want image display).
 
-Updates from v1:
-- Supports storing whole files of unlimited size
-- Supports image display
-- Changes to backend
+Updates from v2:
+- Switched to symmetric keys for improved performance
+- Lock/unlock mechanism to prevent repeated password inputs
+- Data now rinsed several times
 
 See `test()` function at the bottom of vault.py for example usage.
--- vault.py
+++ vault.py
@@ -1,5 +1,5 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP, Salsa20
+from Crypto.Cipher import PKCS1_OAEP, Salsa20, AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 from typing import Union, Iterable
@@ -18,25 +18,20 @@ except ImportError:
 
 class Vault:
     SALT = b'\xad]:\xbf\xcd\xfd\x92\xe2\xe0w\x18\xc0\xf0\x98\xff-\x99J}4\\\x13?Dbd\xb8Wh\xe4\x00H'
-    # N_ENCRYPTIONS = 1 # more than 1 breaks :(
-    N_PRIVKEY_ENCRYPTIONS = 5
+    N_ENCRYPT = 3
+    N_KEY_ENCRYPT = 20
 
-    def __init__(self, name: str, password: str, bits=2048):
-        password = password[:32]
+    STORE_FIELDS = ('name', 'true_key', 'data')
 
-        key = RSA.generate(bits)
+    def __init__(self, name: str, password: str):
+        self.true_key_decrypted = get_random_bytes(32)
+        self.true_key = self.true_key_decrypted
+        pwd_cipher = AES.new((password.encode() + self.SALT)[:32], mode=AES.MODE_ECB)
+        for i in range(self.N_KEY_ENCRYPT):
+            self.true_key = pwd_cipher.encrypt(self.true_key)
 
-        self.private = key.export_key(format='DER')
-        for i in range(self.N_PRIVKEY_ENCRYPTIONS):
-            sym = Salsa20.new((password.encode() + self.SALT)[:32], nonce=get_random_bytes(8))
-            self.private = sym.nonce + sym.encrypt(self.private)
-        self.public = key.publickey().export_key(format='DER')
         self.name = name
-        self.bits = bits
-        self.data = []
-        self.data_names = []
-
-        self.__init_encryptor()
+        self.data: [dict] = []
 
     def __repr__(self):
         return '<Vault %s: %d entries>' % (self.name, len(self.data))
@@ -48,9 +43,8 @@ class Vault:
         Useful if updates to Vault or key generation are made, or you want to change your password.
         """
         new_vault.store_all(old_vault.read_all(old_password))
-
     @staticmethod
-    def from_file(fp: str):
+    def from_file(fp: str, password: str):
         """
         Load vault from file
         """
@@ -58,33 +52,49 @@ class Vault:
             vault_dict = pickle.load(file)
             vault = Vault.__new__(Vault)
             vault.__dict__.update(vault_dict)
-            vault.__init_encryptor()
+            vault.unlock(password)
         return vault
 
-    def __init_encryptor(self):
-        self.encoder = PKCS1_OAEP.new(RSA.import_key(self.public))
-    def __encrypt_parts(self, data: bytes):
-        block_size = self.bits//16
-        blocks = []
-        for block_i in range(len(data)//block_size+int(bool(len(data)%block_size))):
-            block = data[block_i*block_size:(block_i+1)*block_size]
-            blocks.append(self.encoder.encrypt(block))
-        return blocks
-    def __decrypt_parts(self, password: str, data: Iterable[bytes], cipher=None):
-        cipher = cipher or self.__make_decrypter(password)
-        decrypted = []
-        for block in data:
-            decrypted.append(cipher.decrypt(block))
-        return b''.join(decrypted)
+    def unlock(self, password: str):
+        pwd = (password.encode() + self.SALT)[:32]
+        pwd_cipher = AES.new(pwd, mode=AES.MODE_ECB)
+        self.true_key_decrypted = self.true_key
+        for i in range(self.N_KEY_ENCRYPT):
+            self.true_key_decrypted = pwd_cipher.decrypt(self.true_key_decrypted)
+    def lock(self):
+        self.true_key_decrypted = None
+
+    def __init_cipher(self, iv: bytes):
+        if not self.true_key_decrypted:
+            raise ValueError('Vault is locked. Please call vault.unlock()')
+        return AES.new(self.true_key_decrypted, mode=AES.MODE_CBC, iv=iv)
+    def __encrypt(self, data: bytes, iv: bytes, n_times=N_ENCRYPT):
+        print('before pad', data[-32:])
+        data = pad(data, AES.block_size)
+        print('after pad', data[-32:])
+        for _ in range(n_times):
+            cipher = self.__init_cipher(iv)
+            data = cipher.encrypt(data)
+        return data
+    def __decrypt(self, data: bytes, iv: bytes, n_times=N_ENCRYPT):
+        for _ in range(n_times):
+            cipher = self.__init_cipher(iv)
+            data = cipher.decrypt(data)
+        print('decrypted', data[-32:])
+        return unpad(data, AES.block_size)
+
+    def __make_entry(self, data: bytes, name: str = 'Unnamed Data'):
+        iv = get_random_bytes(16)
+        return {'name': name, 'data': self.__encrypt(data, iv), 'iv': iv}
 
     if USE_CV:
-        def disp_image(self, password: str, index: int):
+        def disp_image(self, index: int):
             """
             This method displays an image without ever saving it decrypted to a file
             """
-            imdata = np.frombuffer(self.read_item(password, index), dtype=np.uint8)
+            imdata = np.frombuffer(self.read_item(index), dtype=np.uint8)
             img = cv2.imdecode(imdata, cv2.IMREAD_UNCHANGED)
-            fname = self.data_names[index]
+            fname = self.data[index]['name']
             cv2.imshow(f'vault_display_{fname}', img)
             cv2.setWindowTitle(f'vault_display_{fname}', fname)
             print('Displaying image. Press any key to exit.')
@@ -110,24 +120,13 @@ class Vault:
                         return
         """
 
-    def __make_decrypter(self, password):
-        key = self.private
-        for _ in range(self.N_PRIVKEY_ENCRYPTIONS):
-            sym = Salsa20.new((password.encode() + self.SALT)[:32], nonce=key[:8])
-            key = sym.decrypt(key[8:])
-        #print(key)
-        try:
-            return PKCS1_OAEP.new(RSA.import_key(key))
-        except ValueError:
-            raise ValueError('Wrong password %s' % password)
-
     def ls(self):
-        return f'Vault "{self.name}"\n' + '\n'.join(('\t%d - ' % i) + name for i, name in enumerate(self.data_names))
+        return f'Vault "{self.name}"\n' + '\n'.join(('\t%d - ' % i) + entry['name'] for i, entry in enumerate(self.data))
 
     def rename_vault(self, name):
         self.name = name
     def rename_item(self, index, name):
-        self.data_names[index] = name
+        self.data[index]['name'] = name
 
     def store_file(self, fp:str):
         """
@@ -135,12 +134,12 @@ class Vault:
         """
         self.store_item(open(fp, 'rb').read(), os.path.split(fp)[-1])
 
-    def export_item_into_file(self, password: str, index: int, fp: str):
+    def export_item_into_file(self, index: int, fp: str):
         """
         Exports an item, DECRYPTED
         """
         with open(fp, 'wb') as f:
-            f.write(self.read_item(password, index))
+            f.write(self.read_item(index))
 
     def store_item(self, data: Union[bytes, str], name='Unnamed Data'):
         """
@@ -149,8 +148,7 @@ class Vault:
         print('Encrypting %.1f kB...' % (len(data)/1000))
         if type(data) is str:
             data = data.encode()
-        self.data.append(self.__encrypt_parts(data))
-        self.data_names.append(name)
+        self.data.append(self.__make_entry(data, name))
 
     def store_all(self, data: Iterable[Union[bytes, str]], names=()):
         """
@@ -160,55 +158,46 @@ class Vault:
             if names:
                 self.store_item(item, names[i])
             else:
-                self.store_item(item, 'Unnamed Data')
+                self.store_item(item)
 
-    def read_item(self, password: str, index: int, cipher=None):
+    def read_item(self, index: int):
         """
         Reads item from vault and decrypts.
         """
-        if len(self.data[index]) > 2:
+        if len(self.data[index]) > 1024:
             print('Decrypting large item. This may take a while... ', end='')
-        dec = self.__decrypt_parts(password, self.data[index], cipher=cipher)
-        print('done!')
+        entry = self.data[index]
+        dec = self.__decrypt(entry['data'], entry['iv'])
         return dec
 
-    def read_all(self, password: str):
+    def read_all(self):
         """
         Reads everything from vault, decrypted.
         """
-        cipher = self.__make_decrypter(password)
-        return [self.read_item(password, i, cipher=cipher) for i in range(len(self.data))]
+        return [self.read_item(i) for i in range(len(self.data))]
 
-    def dump(self):
+    def dump(self, overwrite=False):
         """
         Dump to file
         """
         fname = self.name + '.vault'
-
-        if exists(fname):
+        if exists(fname) and not overwrite:
             answer = input('WARNING: a vault named \'%s\' already exists. Overwrite it? (y/n): ' % self.name)
             if answer[0].lower() != 'y':
                 return
-
-        vault_dict = {'name': self.name,
-                      'bits': self.bits,
-                      'private': self.private,
-                      'public': self.public,
-                      'data': self.data,
-                      'data_names': self.data_names}
-
         with open(fname, 'wb') as file:
-            pickle.dump(vault_dict, file)
+            pickle.dump({k:self.__dict__[k] for k in self.STORE_FIELDS}, file)
 
 
 def test():
     vault = Vault('test', 'password')  # creates a new vault 'test' with password 'password'
     vault.store_item('hello world!')  # writes 'hello world!' as a data entry in the vault - no password required, uses public key
-    #vault.store_file('sus_image.png')  # writes a whole file into the vault - no maximum size, as it is broken up into blocks
-    vault.dump()  # dumps the vault to 'test.vault' using pickle
+    vault.store_file('sus_image.png')  # writes a whole file into the vault - no maximum size, as it is broken up into blocks
+    vault.dump(overwrite=True)  # dumps the vault to 'test.vault' using pickle
     print(vault.ls())  # displays a summary of the vault content
-    loaded_vault = Vault.from_file('test.vault')  # reads contents of 'test.vault' into a new vault
-    #loaded_vault.disp_image('password', 1)  # prints all data entries of the new vault - password required, uses private key to decrypt
+    loaded_vault = Vault.from_file('test.vault', 'password')  # reads contents of 'test.vault' into a new vault
+    loaded_vault.read_item(0)
+    loaded_vault.disp_image(1)  # prints all data entries of the new vault - password required, uses private key to decrypt
 
 if __name__ == "__main__":
     test()
